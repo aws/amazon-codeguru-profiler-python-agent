3329fe2bcbeb1c51fad71ba38682e55c469301f6
Mirela Popoveniuc <mirelap@amazon.com>
2021-01-08 11:58:15 +0000

    Use the existing wait_for from help_utils.

--- test/unit/test_profiler_runner.py
+++ test/unit/test_profiler_runner.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
-import time
 from codeguru_profiler_agent.reporter.agent_configuration import AgentConfiguration
 from test.pytestutils import before
+from test.help_utils import wait_for
 from mock import MagicMock
 
 from codeguru_profiler_agent.profiler_runner import ProfilerRunner
@@ -86,17 +86,7 @@ class TestProfilerRunner:
         # calling start in this test, it will start the scheduler and because initial delay is 0 it will execute now
         self.profiler_runner.start()
         # still it is safer to wait until the new config has been applied
-        wait_until(lambda: AgentConfiguration.get().should_profile == False)
+        wait_for(lambda: AgentConfiguration.get().should_profile is False)
 
         assert self.profiler_runner.scheduler._get_next_delay_seconds() == 150
         self.mock_collector.add.assert_not_called()
-
-
-def wait_until(predicate, max_wait_seconds=1, period_seconds=0.1):
-    start = time.time()
-    timeout = start + max_wait_seconds
-    while time.time() < timeout:
-        if predicate():
-            return True
-        time.sleep(period_seconds)
-    raise AssertionError("Predicate was never true after waiting for " + str(max_wait_seconds) + " seconds")
