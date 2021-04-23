a33c28c604960eb4d21f52230da685848b161ec8
Parag Panda <pandpara@amazon.com>
2021-04-23 04:29:30 -0700

    Add back synchronization util

---
+++ codeguru_profiler_agent/utils/synchronization.py
@@ -0,0 +1,15 @@
+import functools
+import threading
+
+
+def synchronized(wrapped):
+    """The missing @synchronized decorator
+
+    https://git.io/vydTA"""
+    _lock = threading.RLock()
+
+    @functools.wraps(wrapped)
+    def _wrapper(*args, **kwargs):
+        with _lock:
+            return wrapped(*args, **kwargs)
+    return _wrapper
