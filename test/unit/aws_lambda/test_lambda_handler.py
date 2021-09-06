e248ac7394b2164f1879eeefa5e233f78749a653
Pierre Marieu <marieup@amazon.com>
2021-09-06 19:39:39 +0200

    Update lambda handler function extraction to work with python 3.9
    When running on lambda functions, the CodeGuru Profiler module should be
    loaded by lambda bootstrap framework instead of the customer's code.
    Then we load the customer's handler function by calling the bootstarp
    code directly.
    
    We currently handle python 3.8, 3.7 and 3.6 (with some tweeks), however
    for python 3.9 the lambda bootstrap module is now delegating to the
    [runtime interface client](https://github.com/aws/aws-lambda-python-runtime-interface-client)
    so our code must be changed to also work with this.
    
    This changes moves the code about finding the proper function that
    can load a handler function into a separate method. Unit tests are
    updated to cope with this.

--- test/unit/aws_lambda/test_lambda_handler.py
+++ test/unit/aws_lambda/test_lambda_handler.py
@@ -11,6 +11,7 @@ def handler_function(event, context):
 
 
 init_handler_has_been_called = False
+python36_extractor_has_been_called = False
 
 
 def init_handler():
@@ -27,8 +28,16 @@ class BootstrapModuleMock:
 class BootstrapPython36ModuleMock:
     # for python3.6 version of lambda runtime bootstrap
     def _get_handlers(self, handler, mode, invokeid):
+        global python36_extractor_has_been_called
+        python36_extractor_has_been_called = True
         if handler == "handler_module.handler_function" and mode == "event":
             return init_handler, handler_function
+
+
+class RicBootstrapModuleMock:
+    def _get_handler(self, handler):
+        if handler == "handler_module.handler_function":
+            return handler_function
 
 
 class TestLambdaHandler:
@@ -53,48 +62,86 @@ class TestLambdaHandler:
             assert lambda_handler_module.call_handler(event="expected_event",
                                                       context="expected_context") == "expected result"
 
-    class TestLoadModuleFunction:
-        class TestWhenPython38LambdaBootstrapCalls:
-            class TestWhenHandlerEnvIsSetProperly:
-                @before
-                def before(self):
-                    self.bootstrap = BootstrapModuleMock()
-                    self.env = {"HANDLER_ENV_NAME_FOR_CODEGURU": "handler_module.handler_function"}
+    class TestGetHandlerExtractor:
+        class TestWhenRicIsAvailable:
+            @pytest.fixture(autouse=True)
+            def around(self):
+                # simulate that we are in a lambda environment where the awslambdaric.bootstrap module is available
+                self.module_available = RicBootstrapModuleMock()
+                sys.modules['awslambdaric.bootstrap'] = self.module_available
+                yield
+                del sys.modules['awslambdaric.bootstrap']
 
-                def test_it_returns_the_handler_function(self):
-                    from codeguru_profiler_agent.aws_lambda.lambda_handler import load_handler
-                    assert load_handler(self.bootstrap, self.env) == handler_function
+            def test_it_loads_the_ric_module_code(self):
+                from codeguru_profiler_agent.aws_lambda.lambda_handler import get_lambda_handler_extractor
+                result = get_lambda_handler_extractor()
+                assert result == self.module_available._get_handler
 
-                def test_it_resets_handler_env_variable(self):
-                    from codeguru_profiler_agent.aws_lambda.lambda_handler import load_handler
-                    load_handler(self.bootstrap, self.env)
-                    assert self.env['_HANDLER'] == "handler_module.handler_function"
+        class TestWhenLambdaBootstrapIsAvailable:
+            @pytest.fixture(autouse=True)
+            def around(self):
+                # simulate that we are in a lambda environment where the awslambdaric.bootstrap module is not available
+                # but bootstrap from lambda is available.
+                self.module_available = BootstrapModuleMock()
+                if 'awslambdaric.bootstrap' in sys.modules:
+                    del sys.modules['awslambdaric.bootstrap']
+                sys.modules['bootstrap'] = self.module_available
+                yield
+                del sys.modules['bootstrap']
 
-            class TestWhenHandlerEnvIsMissing:
-                @before
-                def before(self):
-                    self.bootstrap = BootstrapModuleMock()
-                    self.env = {}
-
-                def test_it_throws_value_error(self):
-                    with pytest.raises(ValueError):
-                        from codeguru_profiler_agent.aws_lambda.lambda_handler import load_handler
-                        load_handler(self.bootstrap, self.env)
+            def test_it_loads_the_lambda_module_code(self):
+                from codeguru_profiler_agent.aws_lambda.lambda_handler import get_lambda_handler_extractor
+                result = get_lambda_handler_extractor()
+                assert result == self.module_available._get_handler
 
         class TestWhenPython36LambdaBootstrapCalls:
             class TestWhenHandlerEnvIsSetProperly:
-                @before
-                def before(self):
-                    self.bootstrap = BootstrapPython36ModuleMock()
-                    self.env = {"HANDLER_ENV_NAME_FOR_CODEGURU": "handler_module.handler_function"}
+                @pytest.fixture(autouse=True)
+                def around(self):
+                    # simulate that we are in a lambda environment where the awslambdaric.bootstrap module is available
+                    sys.modules['bootstrap'] = BootstrapPython36ModuleMock()
                     global init_handler_has_been_called
                     init_handler_has_been_called = False
+                    global python36_extractor_has_been_called
+                    python36_extractor_has_been_called = False
+                    yield
+                    del sys.modules['bootstrap']
 
-                def test_it_returns_the_handler_function(self):
-                    from codeguru_profiler_agent.aws_lambda.lambda_handler import load_handler
-                    assert load_handler(self.bootstrap, self.env) == handler_function
+                def test_it_uses_the_old_bootstrap_code(self):
+                    from codeguru_profiler_agent.aws_lambda.lambda_handler import get_lambda_handler_extractor
+                    # call extractor
+                    get_lambda_handler_extractor()("handler_module.handler_function")
+                    assert python36_extractor_has_been_called
 
                 def test_it_calls_the_init_handler(self):
-                    from codeguru_profiler_agent.aws_lambda.lambda_handler import load_handler
-                    load_handler(self.bootstrap, self.env)
+                    from codeguru_profiler_agent.aws_lambda.lambda_handler import get_lambda_handler_extractor
+                    # call extractor
+                    get_lambda_handler_extractor()("handler_module.handler_function")
                     assert init_handler_has_been_called
+
+    class TestLoadHandlerFunction:
+        class TestWhenHandlerEnvIsSetProperly:
+            @before
+            def before(self):
+                self.extractor = BootstrapModuleMock()._get_handler
+                self.env = {"HANDLER_ENV_NAME_FOR_CODEGURU": "handler_module.handler_function"}
+
+            def test_it_returns_the_handler_function(self):
+                from codeguru_profiler_agent.aws_lambda.lambda_handler import load_handler
+                assert load_handler(self.extractor, self.env) == handler_function
+
+            def test_it_resets_handler_env_variable(self):
+                from codeguru_profiler_agent.aws_lambda.lambda_handler import load_handler
+                load_handler(self.extractor, self.env)
+                assert self.env['_HANDLER'] == "handler_module.handler_function"
+
+        class TestWhenHandlerEnvIsMissing:
+            @before
+            def before(self):
+                self.extractor = BootstrapModuleMock()._get_handler
+                self.env = {}
+
+            def test_it_throws_value_error(self):
+                with pytest.raises(ValueError):
+                    from codeguru_profiler_agent.aws_lambda.lambda_handler import load_handler
+                    load_handler(self.extractor, self.env)
