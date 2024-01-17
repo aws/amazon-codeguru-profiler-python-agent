98c0f1c1d1805595ce97cc1baf7f8f7c50105b24
Henry Huynh <82241632+henryhuynh210@users.noreply.github.com>
2024-01-17 05:52:03 -0800

    Update test_with_timer.py

--- test/unit/metrics/test_with_timer.py
+++ test/unit/metrics/test_with_timer.py
@@ -16,7 +16,7 @@ class TargetClass:
     @with_timer(metric_name="test-foo-cpu", measurement="cpu-time")
     def foo_cpu(self):
         # Run something to make sure the cpu clock does tick (https://bugs.python.org/issue37859)
-        if sys.version_info >= (3, 6):
+        if sys.version_info > (3, 6):
             sys.set_int_max_str_digits(0)
         len(str(2 ** 500_000))
         return
