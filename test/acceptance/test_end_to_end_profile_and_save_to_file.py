4938b3dcbbf728fa704d53e4e22cd5f0e005ba89
John Palevich <jpalevic@amazon.com>
2024-02-14 15:01:31 -0800

    format string

--- test/acceptance/test_end_to_end_profile_and_save_to_file.py
+++ test/acceptance/test_end_to_end_profile_and_save_to_file.py
@@ -78,7 +78,7 @@ class TestEndToEndProfileAndSaveToFile:
                 file_content = profiling_result_file.read()
 
             try: 
-                print(file_content)
+                print(f"Content: \"%s\"", file_content)
                 resulting_json = json.loads(file_content)
             except json.JSONDecodeError as e:
                 raise
